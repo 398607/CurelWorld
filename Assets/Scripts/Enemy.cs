0a3a840415cb441ed0421a13a355b4354295483a
nagizero <nagizero@foxmail.com>
2015-10-01 12:17:50 +0800

    First Version

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Random = UnityEngine.Random;
+
+public class Enemy : MovingObject {
+
+	public int playerDamage;
+
+	private Animator animator;
+	private Transform target;
+	private bool skipMove;
+
+	private int[] dx;
+	private int[] dy;
+
+	// Use this for initialization
+	protected override void Start() {
+		GameManager.instance.AddEnemyToList(this);
+		animator = GetComponent<Animator>();
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+
+		dx = new int[] { 0, 0, 1, -1 };
+		dy = new int[] { 1, -1, 0, 0 };
+
+		base.Start();
+	}
+
+	protected override void AttemptMove<T>(int xDir, int yDir) {
+		if (skipMove) {
+			skipMove = false;
+			return;
+		}
+		base.AttemptMove<T>(xDir, yDir);
+
+		skipMove = true;
+	}
+
+	public void MoveEnemy() {
+		Vector3 tmpPosition = transform.position;
+		float minDistance = (target.position - tmpPosition).sqrMagnitude;
+		int minIndex = -1; // do not move
+		
+		for (int dir = 0; dir < 4; dir ++) {
+			tmpPosition.x += dx[dir];
+			tmpPosition.y += dy[dir];
+			float distance = (target.position - tmpPosition).sqrMagnitude;
+			if (distance < minDistance) {
+				minDistance = distance;
+				minIndex = dir;
+			}
+			tmpPosition.x -= dx[dir];
+			tmpPosition.y -= dy[dir];
+		}
+
+		AttemptMove<Player>(dx[minIndex], dy[minIndex]);
+	}
+
+	protected override void OnCantMove<T>(T component) {
+		Player hitPlayer = component as Player;
+
+		animator.SetTrigger("enemyAttack");
+
+		hitPlayer.LoseFood(playerDamage + Random.Range(-5, 5));
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Player : MovingObject {
+
+	public int wallDamage = 1;
+	public int pointsPerFood = 10;
+	public int pointsPerSoda = 20;
+	public float restartLevelDelay = 0.5f;
+	public Text foodText;
+
+	private Animator animator;
+	private int food;
+
+	// Use this for initialization
+	protected override void Start () {
+		animator = GetComponent<Animator>();
+
+		food = GameManager.instance.playerFoodPoints;
+		foodText.text = "Food: " + food;
+
+		base.Start();
+	}
+
+	private void OnDisable() {
+		GameManager.instance.playerFoodPoints = food;
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		if (!GameManager.instance.playersTurn)
+			return;
+
+		int horizontal = 0, vertical = 0;
+
+		horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+		vertical = (int)(Input.GetAxisRaw("Vertical"));
+		
+
+		if (horizontal != 0)
+			vertical = 0;
+
+		if (horizontal != 0 || vertical != 0) {
+			AttemptMove<Wall>(horizontal, vertical);
+		}
+	}
+
+	protected override void AttemptMove<T>(int xDir, int yDir) {
+		food--;
+		foodText.text = "Food: " + food;
+
+		base.AttemptMove<T>(xDir, yDir);
+
+		// RaycastHit2D hit;
+
+		// Move(xDir, yDir, out hit);
+
+		CheckIfGameOver();
+
+		Debug.Log("Player`s turn ends, food " + food.ToString());
+
+		GameManager.instance.playersTurn = false;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Exit") {
+			Invoke("Restart", restartLevelDelay);
+			enabled = false;
+        }
+		else if (other.tag == "Food") {
+			int add = pointsPerFood + Random.Range(-5, 5);
+			food += add;
+			foodText.text = "+ " + add + " Food: " + food;
+			other.gameObject.SetActive(false);
+		}
+		else if (other.tag == "Soda") {
+			int add = pointsPerSoda + Random.Range(-10, 10);
+			food += add;
+			foodText.text = "+ " + add + " Food: " + food;
+
+			other.gameObject.SetActive(false);
+		}
+	}
+
+	protected override void OnCantMove<T>(T component) {
+		Wall hitWall = component as Wall;
+		hitWall.DamageWall(wallDamage);
+		Debug.Log("Wall hit");
+		animator.SetTrigger("playerChop");
+	}
+
+	private void Restart() {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	public void LoseFood(int foodLoss) {
+		animator.SetTrigger("playerHit");
+		food -= foodLoss;
+		foodText.text = "- " + foodLoss + " Food: " + food;
+		CheckIfGameOver();
+	}
+
+	private void CheckIfGameOver() {
+		if (food <= 0)
+			GameManager.instance.GameOver();
+	}
+}
